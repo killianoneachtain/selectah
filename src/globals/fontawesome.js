4efcc644d37cf5a3c9ee4e2b7f71eefb081b8dbd
Killian O'Neachtain <killianoneachtain@hotmail.com>
2020-11-17 18:20:43 +0000

    Initial Commit

---
+++ src/globals/fontawesome.js
@@ -0,0 +1,11 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faStar,
+  faFilm,
+  faVideo,
+  faHome,
+  faCalendar,
+  faArrowCircleLeft
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add( faStar, faFilm, faVideo, faHome, faCalendar, faArrowCircleLeft);
