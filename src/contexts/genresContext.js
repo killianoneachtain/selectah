3a71dc224f72a5eb6826c776e7149f2f37e18a88
Killian O'Neachtain <killianoneachtain@hotmail.com>
2021-03-07 14:44:23 +0000

    Refactoring of loop to allow the artist name and song title props to be passed to getBPM component

--- src/contexts/genresContext.js
+++ src/contexts/genresContext.js
@@ -4,7 +4,7 @@ import { getGenres } from '../api/Discogs_api'
 export const GenresContext = createContext(null)
 
 const GenresContextProvider = props => {
-    const [genres, setGenres] = useState([{ id: "0", name: "All" }]);
+    const [genres, setGenres] = useState([{ id: 0, name: "All" }]);
     
     useEffect(() => {
       getGenres().then(allGenres => {
@@ -12,10 +12,10 @@ const GenresContextProvider = props => {
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-
+    
     return (
         <GenresContext.Provider
-          value={{ genres }}  >
+          value={{ genres }}  >            
           {props.children}
         </GenresContext.Provider>    
     )
